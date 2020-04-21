d773f63bda21a321c1071dab523de9746b6a636f
Igor Gomes Lemes <igor.lemes@live.com>
2020-04-21 19:06:00 -0300

    2.3 Implemented user repository

--- src/com/twu/biblioteca/database/UserRepository.java
+++ src/com/twu/biblioteca/database/UserRepository.java
@@ -1,4 +1,17 @@
 package com.twu.biblioteca.database;
 
-public class UserRepository {
+import com.twu.biblioteca.User;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public final class UserRepository {
+    public List <User> bibliotecaUsers = new ArrayList<User>(Arrays.asList(
+            new User("888-8888","test","Kevin Gardner","kevin@gardner.com","99937899"),
+            new User("123-4567","123","Laura Palmer", "laura_palmer@gmaik.com","87675423")
+    ));
+
+    
 }
